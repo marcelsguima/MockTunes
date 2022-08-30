fc40528774630f0d778746c6dfd9b31e838a37cd
marceslguima <marcelsguima@gmail.com>
2022-08-30 16:41:18 -0300

    Req 1 lint fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -13,22 +12,22 @@ import NotFound from './pages/NotFound';
 class App extends React.Component {
   render() {
     return (
-    <div>
-      <p>TrybeTunes!</p>
-      <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component= {Login} />
-        <Route exact path="/search" component = {Search} />
-        <Route exact path="/album/:id" component = {Album} />
-        <Route exact path="/favorites" component = {Favorites} />
-        <Route exact path="/profile" component = {Profile} />
-        <Route exact path="/profile/edit" component = {ProfileEdit} />
-        <Route exact path="*" component = {NotFound} />
-        
-      </Switch>
-      </BrowserRouter>
+      <div>
+        <p>TrybeTunes!</p>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route exact path="/search" component={ Search } />
+            <Route exact path="/album/:id" component={ Album } />
+            <Route exact path="/favorites" component={ Favorites } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route exact path="/profile/edit" component={ ProfileEdit } />
+            <Route exact path="*" component={ NotFound } />
+
+          </Switch>
+        </BrowserRouter>
       </div>
-    ) 
+    );
   }
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,22 @@ import React from 'react';
 class Login extends React.Component {
   render() {
     return (
-      <div data-testid="page-login">Login test</div>
+      <div data-testid="page-login">
+        <p>Login Page</p>
+        {/* <form>
+          <label htmlFor="cardName">
+            Nome da carta:
+            <input
+              id="cardName"
+              data-testid="name-input"
+              type="text"
+              name="cardName"
+              value={ cardName }
+              onChange={ onInputChange }
+            />
+          </label>
+        </form> */}
+      </div>
     );
   }
 }
